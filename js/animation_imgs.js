13911fd19eb5ccb5c3d6f6ae57b86dfb5c804a40
ellvenro <lidiya.ellie@yandex.ru>
2022-07-23 17:34:40 +0300

    Создание анимации третьего блока

--- js/animation_imgs.js
+++ js/animation_imgs.js
@@ -1,4 +1,5 @@
-const imgs = document.querySelectorAll('.img_long4');
+const imgs = document.querySelectorAll('.img_anim');
+const texts = document.querySelectorAll('.text_anim');
 
 let index = -1;
 let indexPrev = -1;
@@ -11,8 +12,15 @@ function activeImg(n, np)
     img.classList.remove('noactive');
   }
   imgs[n].classList.add('active');
+
   if (np != imgs.length - 1)
     imgs[np].classList.add('noactive');
+  else
+  {
+    for(text of texts)
+      text.classList.remove('active');
+  }
+  texts[n].classList.add('active');
 }
 
 function nextImg()
