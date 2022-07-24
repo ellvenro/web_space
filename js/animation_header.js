00e8c045fd38183f77d2ecb9d45623e5c07b84e5
ellvenro <lidiya.ellie@yandex.ru>
2022-07-24 13:02:04 +0300

    Анимация загрузки страницы

--- js/animation_header.js
+++ js/animation_header.js
@@ -3,6 +3,39 @@ const block = document.querySelectorAll('.back'),
 let scale = -1,
   scalePrev = -1;
 
+//Функция задержки
+function sleep(ms)
+{
+  return new Promise(
+    resolve => setTimeout(resolve, ms)
+  );
+}
+
+//Начальная заставка
+async function animHead()
+{
+  //Анимация точек в начате
+  const circles = document.querySelectorAll('.circle');
+
+  for(let i = 0; i < 3; i++)
+  {
+    circles[0].classList.add('active');
+    circles[1].classList.remove('active');
+    circles[2].classList.add('active');
+    await sleep(300);
+    circles[0].classList.remove('active');
+    circles[1].classList.add('active');
+    circles[2].classList.remove('active');
+    await sleep(300);
+  }
+
+  //Появление основных объектов заголовка
+  document.getElementById('circles').classList.add("noactive");
+  const head = document.querySelectorAll('header');
+  head[0].classList.remove('active');
+}
+
+//Анимация заголовка при скролле
 function animEvent()
 {
   const rect = block[0].getBoundingClientRect();
@@ -21,12 +54,6 @@ function animEvent()
   scalePrev = scale;
 }
 
-function animHead()
-{
-  const head = document.querySelectorAll('header');
-  head[0].classList.remove('active');
-}
-
-setTimeout(() => {animHead()}, 1000);
-
+//setTimeout(() => {animHead()}, 1000);
+animHead();
 window.addEventListener('scroll', animEvent);
